505f76a05ebf85027a947cf8adf666051cbde089
Sasha <sasha@MacBook-Pro-Sasha.local>
2026-02-18 12:49:09 +0300

    otladka

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
+import { ru } from "date-fns/locale";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
-function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
+function Calendar({ className, classNames, showOutsideDays = true, locale, weekStartsOn, ...props }: CalendarProps) {
   return (
     <DayPicker
+      locale={locale ?? ru}
+      weekStartsOn={weekStartsOn ?? 1}
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
